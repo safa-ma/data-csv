9651b0f33883585259fa1e28b182e0636a3a69de
safa albukhari <bukharisafam@gmail.com>
2025-09-02 14:25:06 +0300

    progress on CSV: XXX test passing

--- phone_book.py
+++ phone_book.py
@@ -1,1 +1,14 @@
+import csv
 
+#with open('data/phone_book.csv', mode='r') as csv_file:
+#   csv_reader = csv.reader(csv_file)
+#    line_count = 0
+#   for row in csv_reader:
+#        print(row)
+#        line_count += 1
+
+with open('data/phone_book.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    for row in csv_reader:
+       print(f"{row['last_name']}: {row['phone_number']}")
